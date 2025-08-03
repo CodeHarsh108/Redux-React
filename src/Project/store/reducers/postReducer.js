919d0af9ede78bdda85af59ac3c531b02f50e8bc
Harsh Patil <itsharshhh6@gmail.com>
2025-08-03 15:46:28 +0530

    Reducers

--- src/Project/store/reducers/postReducer.js
+++ src/Project/store/reducers/postReducer.js
@@ -7,10 +7,23 @@ const initialState = {
 export const postReducer = (state = initialState, action) => {
     switch (action.type){
         case "FETCH_POST_REQUEST":
-
+            return {
+                ...state,
+                loading: true,
+            }
         case "FETCH_POST_SUCCESS":
+            return {
+                ...state,
+                loading: false,
+                posts: action.payload, 
+            }
             
         case "FETCH_POST_FAILURE":
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            }
 
         default:
             return state;
