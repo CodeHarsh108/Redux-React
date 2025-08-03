9d10f375bce02d09c2abb3d745cb023f5203b9e1
Harsh Patil <itsharshhh6@gmail.com>
2025-08-03 09:21:20 +0530

    Counter Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
    <div>
     <h1>Welcome</h1>
+    <p>This is a simple React application.</p>
+    <Counter />
+    <p>Enjoy exploring!</p>
+    <p>Have a great day!</p>
+    <p>Feel free to modify the code.</p>
+    <p>Happy coding!</p>
    </div>
   )
 }
