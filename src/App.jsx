2308c3efdb97bd5244c436e2c5fcf8c09533a90a
Harsh Patil <itsharshhh6@gmail.com>
2025-08-03 10:24:44 +0530

    Main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Counter from './COunter'
 
 function App() {
 
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,12 @@
+import { use } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 const Counter  = () => {
+    const count = useSelector((state) => state.count);
+    const dispatch = useDispatch();
     return(
         <div>
-            <h1>Counter Component</h1>
+            <h1>Counter : {count}</h1>
         </div>
     )
 };
